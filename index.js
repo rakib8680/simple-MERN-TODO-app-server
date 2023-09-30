1ae23fc6958ee6ff9d6981ad40292ef645ad9694
rakib8680 <rakib.cst.1st@gmail.com>
2023-09-30 17:26:02 +0600

    delete task api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -46,15 +46,26 @@ async function run() {
 
 
 
-
+        // add task 
         app.post('/tasks', async (req, res) => {
             const task = req.body;
             const result = await tasksCollection.insertOne(task);
             res.send(result);
         });
 
-        app.get('/tasks', async (req, res) => {
+
+        // get all task 
+        app.get('/allTasks', async (req, res) => {
             const result = await tasksCollection.find().toArray();
+            res.send(result);
+        });
+
+
+        // delete task 
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await tasksCollection.deleteOne(query);
             res.send(result);
         })
 
